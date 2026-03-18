0e15a6d534515fbc107eb31cb0a8973784768c7f
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-03-18 15:23:00 +0800

    客户催单业务代码

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -94,6 +94,12 @@ public interface OrderService {
     void complete(Long id);
 
     /**
+     * 客户催单
+     * @param id
+     */
+    void reminder(Long id);
+
+    /**
      * 支付成功，修改订单状态
      * @param outTradeNo
      */
